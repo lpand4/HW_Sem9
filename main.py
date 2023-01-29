08f2bf9c2005d200e80616de6cd9d2bf524483b7
Vladislav <vlad-pugovkin2@tandex.ru>
2023-01-29 22:11:19 +0300

    add easy tg_bot

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from isOdd import isOdd
+import emoji
 
 
-print(isOdd(0))
+print(isOdd(1))
+print(isOdd(4))
+
+print(emoji.emojize('Python is :thumbs_up:'))
+print(emoji.emojize('Python is :thumbsup:', language='alias'))
+print(emoji.demojize('Python is 👍'))
+print(emoji.emojize("Python is fun :red_heart:"))
+print(emoji.emojize("Python is fun :red_heart:", variant="emoji_type"))
+print(emoji.is_emoji("👍"))
+print(emoji.emojize('Python es :pulgar_hacia_arriba:', language='es'))
